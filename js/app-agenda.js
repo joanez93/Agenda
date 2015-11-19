94474e7b945a188bc6dc0cd0e53846c0ca2c1313
Joanez <joanez-dev@izzyweb.com.br>
2015-11-18 22:48:45 -0200

    Função Adicionar Contato

--- js/app-agenda.js
+++ js/app-agenda.js
@@ -4,4 +4,11 @@ app.controller("appAgendaCtrl", function($scope){
 		{name: "Joanez Andrades", ddd: 45, phone: "8830-4602", city: "Santa Lúcia", op: "Claro"},
 		{name: "Grazi Massafera", ddd: 35, phone: "99922-4223", city: "São Paulo", op: "Tim"}
 	];
+	$scope.operadoras = ["Claro", "Oi", "Vivo", "Tim"];
+
+	$scope.adcContato = function (contato){
+		$scope.agenda.push(angular.copy(contato));
+		delete $scope.contato;
+		$scope.contatoForm.$setPristine;
+	};
 });
